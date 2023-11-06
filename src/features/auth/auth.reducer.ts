bae0615e758dc797ebb9f6ec6e0515767e1ceb0a
Alexander Korobov <dktwho@mail.ru>
2023-11-06 18:04:51 +0300

    add to login in authReducer validate for incorrect password

--- src/features/auth/auth.reducer.ts
+++ src/features/auth/auth.reducer.ts
@@ -38,7 +38,12 @@ export const login = createAppAsyncThunk<{
             dispatch(appActions.setAppStatus({status: "succeeded"}));
             return {isLoggedIn: true}
         } else {
-             handleServerAppError(res.data, dispatch, false);
+            // ❗ Если у нас fieldsErrors есть значит мы будем отображать эти ошибки
+            // в конкретном поле в компоненте (пункт 7)
+            // ❗ Если у нас fieldsErrors нету значит отобразим ошибку глобально
+            const isShowAppError = !res.data.fieldsErrors.length
+            handleServerAppError(res.data, dispatch, isShowAppError);
+             // handleServerAppError(res.data, dispatch, false);
             return rejectWithValue(res.data)
         }
     } catch (error) {
